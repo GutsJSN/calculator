384fae0b0b5d9f60798a8298fcf4d90796a54ade
GutsJSN <jaysonbhat@gmail.com>
2024-05-04 15:23:15 +0530

    finish project

---
+++ calc.js
@@ -0,0 +1,44 @@
+const calcInput = document.querySelector(".calc-input");
+const allButtons = document.querySelectorAll(".button");
+const operators = ["*","/","-","+"];
+allButtons.forEach(button =>{
+    button.addEventListener("click",(e)=>{
+        const value=e.target.dataset.value;
+        
+        if(value==="="){
+            if(calcInput.innerText.includes("/")){
+                const split = calcInput.innerText.split("/");
+                operate("/",split[0],split[1]);
+            }
+            if(calcInput.innerText.includes("*")){
+                const split = calcInput.innerText.split("*");
+                operate("*",split[0],split[1]);
+            }
+            if(calcInput.innerText.includes("-")){
+                const split = calcInput.innerText.split("-");
+                operate("-",split[0],split[1]);
+            }
+            if(calcInput.innerText.includes("+")){
+                const split = calcInput.innerText.split("+");
+                operate("+",split[0],split[1]);
+            }
+        }
+        calcInput.innerText +=value;
+        if(value==="clear")
+        calcInput.innerText="";
+    }
+)
+})
+const operate = (operator,value1,value2)=>{
+    const num1 = Number(value1);
+    const num2 = Number(value2);
+    if(operator==="/")
+    calcInput.innerText =num1/num2;
+    if(operator==="*")
+    calcInput.innerText =num1*num2;
+    if(operator==="-")
+    calcInput.innerText =num1-num2;
+    if(operator==="+")
+    calcInput.innerText =num1+num2;
+
+}
